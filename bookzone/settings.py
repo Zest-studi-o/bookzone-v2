0448dc5eb9587ec7a250b5b39bb710d0934a8fc3
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2024-02-07 23:40:22 +0000

    Add allauth & base templates

--- bookzone/settings.py
+++ bookzone/settings.py
@@ -88,6 +88,8 @@ WSGI_APPLICATION = 'bookzone.wsgi.application'
 
 SITE_ID = 1
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
