084d05be27eee54b0f7ffc48701dd55aa474ca2a
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2024-02-12 00:25:39 +0000

    Add books views & template

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, Book, Category,Transaction, Review
+from .models import CustomUser, Book, Category, Transaction, Review
 
 # Register your models here.
 
