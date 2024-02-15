3f91815e3a54d92c0efb2a76ecd513db1a75ae2b
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2024-02-15 12:04:54 +0000

    Add queries & categories filters

--- books/views.py
+++ books/views.py
@@ -11,6 +11,22 @@ def all_books(request):
     books = Book.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                books = books.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            books = books.order_by(sortkey)
 
     if request.GET:
         if 'category' in request.GET:
@@ -23,14 +39,18 @@ def all_books(request):
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
+                return redirect(reverse('books'))
             
             queries = Q(title__icontains=query) | Q(description__icontains=query)
             books = books.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'books': books,
         'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'books/books.html', context)
