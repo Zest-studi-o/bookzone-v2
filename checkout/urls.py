de64d47b1d5d664d0d17332288eccec73dbf70de
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2024-02-20 22:56:53 +0000

    Add checkout success page

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,24 +2,9 @@ from django.urls import path
 from . import views
 from .webhooks import webhook
 
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.checkout, name='checkout')
-]
-
-
-"""
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    
-    path('checkout_success/<order_number>',
-         views.checkout_success,
-         name='checkout_success'),
-    path('cache_checkout_data/',
-         views.cache_checkout_data,
-         name='cache_checkout_data'),
+    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
- 
-]   """
+]
