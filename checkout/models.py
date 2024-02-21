b46315ce9659359019c1e54cac3b43e2341e807a
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2024-02-21 06:57:23 +0000

    Fix errors with custom user & migrate profiles

--- checkout/models.py
+++ checkout/models.py
@@ -6,17 +6,17 @@ from django.conf import settings
 from django_countries.fields import CountryField
 # Internal imports
 from books.models import Book
-#from profiles.models import UserProfile
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=320, null=False, editable=False)
     #to add user profiles or custom users UserProfile
-    '''
+    
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
                                      null=True, blank=True,
                                      related_name='orders')
-    '''
+    
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=50, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
