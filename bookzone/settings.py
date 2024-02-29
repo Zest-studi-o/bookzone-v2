12fcd5d97398b788b30e9130b6de5a2f17e74c40
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2024-02-29 11:27:26 +0000

    Test 404 page after disabling debug

--- bookzone/settings.py
+++ bookzone/settings.py
@@ -27,6 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# testing 404
 #DEBUG = False test , remove DEVELOPMENT from env.py and heroku config vars
 #DEBUG = 'DEVELOPMENT' in os.environ
 #DEBUG = False
